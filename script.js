84ce83c317ce22977dd04eb5e743220437bac274
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-02-18 12:21:22 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function Track1togg() {
+  var x = document.getElementById("Track1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
