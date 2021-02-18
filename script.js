cc3e37223dbde9c8300ce74d0a8b62b0ee2169df
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-02-18 12:30:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function Track1togg() {
-  var x = document.getElementById("Track1");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  var T1 = document.getElementById("Track1");
+  if (T1.style.display === "none") {
+    T1.style.display = "block";
   } else {
-    x.style.display = "none";
+    T1.style.display = "none";
   }
 }
 function Track2togg() {
-  var x = document.getElementById("Track2");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  var T2 = document.getElementById("Track2");
+  if (T2.style.display === "none") {
+    T2.style.display = "block";
   } else {
-    x.style.display = "none";
+    T2.style.display = "none";
   }
 }
