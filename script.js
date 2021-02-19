bf541bf04baa52e16243ee4f464d467dbbc5af9f
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-02-19 13:34:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-function Track1togg() {var T1 = document.getElementById("Track1"); if (T1.style.display === "none") {T1.style.display = "block";} else {T1.style.display = "none";}}function Track2togg() {var T2 = document.getElementById("Track2"); if (T2.style.display === "none") {T2.style.display = "block";} else {T2.style.display = "none";}}function Track3togg() {var T3 = document.getElementById("Track3"); if (T3.style.display === "none") {T3.style.display = "block";} else {T3.style.display = "none";}}function Track4togg() {var T4 = document.getElementById("Track4"); if (T4.style.display === "none") {T4.style.display = "block";} else {T4.style.display = "none";}}
+function Track1togg() {
+  var T1 = document.getElementById("Track1"); 
+  if (T1.style.display === "none") {
+    T1.style.display = "block";
+  } else {
+    T1.style.display = "none";
+  }
+}
+function Track2togg() {
+  var T2 = document.getElementById("Track2");
+  if (T2.style.display === "none") {
+    T2.style.display = "block";
+  } else {
+    T2.style.display = "none";
+  }
+}
+function Track3togg() {
+  var T3 = document.getElementById("Track3");
+  if (T3.style.display === "none") {
+    T3.style.display = "block";
+  } else {
+    T3.style.display = "none";
+  }
+}
+function Track4togg() {
+  var T4 = document.getElementById("Track4");
+  if (T4.style.display === "none") {
+    T4.style.display = "block";
+  } else {
+    T4.style.display = "none";
+  }
+}
+function Track5togg() {
+  var T5 = document.getElementById("Track5");
+  if (T5.style.display === "none") {
+    T5.style.display = "block";
+  } else {
+    T5.style.display = "none";
+  }
+}
