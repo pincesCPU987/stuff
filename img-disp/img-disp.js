d4069046773931dfaaf99d0a856e6e8aa2dc7856
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-09-16 12:37:51 -0400

    Update img-disp.js

--- img-disp/img-disp.js
+++ img-disp/img-disp.js
@@ -1,4 +1,4 @@
-var loadImg = function(eventImg) {
-	var image = document.getElementById('outputImg');
-	image.src = URL.createObjectURL(eventImg.target.files[0]);
+var loadFile = function(event) {
+	var image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
 }
