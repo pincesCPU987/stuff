373879dce2483b5f5149eda175b10a7870debdaa
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-09-16 11:59:56 -0400

    Update img-disp.js

--- img-disp/img-disp.js
+++ img-disp/img-disp.js
@@ -2,3 +2,7 @@ var loadImg = function(event) {
 	var image = document.getElementById('outputImg');
 	image.src = URL.createObjectURL(event.target.files[0]);
 }
+var loadAud = function(event) {
+	var audio = document.getElementById('outputAud');
+	audio.src = URL.createObjectURL(event.target.files[0]);
+}
