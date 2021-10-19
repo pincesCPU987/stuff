ab75df6043d27c789455e92ff032eb55fc35fe41
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-10-19 11:47:16 -0400

    Update calc.js

--- calc/calc.js
+++ calc/calc.js
@@ -12,3 +12,27 @@ encodeURIComponent(text));
 
   document.body.removeChild(pom);
 }
+function calculate(){
+  var calc1val = document.getElementById("calc1");
+  var calc2val = document.getElementById("calc2");
+  var oper = document.getElementById("operation");
+  if(oper.value == "choose"){
+    alert("Please choose an operation.");
+  } else if(oper.value == "plus"){
+    var answer = calc1val.value + calc2val.value;
+    alert(calc1val.value + " + " + calc2val.value + " = " + answer + " (This text can be copied into the text box where indicated.)");
+  } else if(oper.value == "minus"){
+    var answer = calc1val.value - calc2val.value;
+    alert(calc1val.value + " - " + calc2val.value + " = " + answer + " (This text can be copied into the text box where indicated.)");
+  } else if(oper.value == "times"){
+    var answer = calc1val.value * calc2val.value;
+    alert(calc1val.value + " × " + calc2val.value + " = " + answer + " (This text can be copied into the text box where indicated.)");
+  } else {
+    if(calc2val.value != 0){
+      var answer = calc1val.value / calc2val.value;
+      alert(calc1val.value + " ÷ " + calc2val.value + " = " + answer + " (This text can be copied into the text box whwere indicated.)");
+    } else {
+      alert("The second value cannot be zero when dividing.");
+    }
+  }
+}
