076c985e9ced8a4b127068c31c2bae95dbc06f92
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-10-29 11:23:59 -0400

    Update submit-form.js

--- submit-form.js
+++ submit-form.js
@@ -1,4 +1,4 @@
-function doc_keyUp(e) {
+function doc_keyUp(formName,e) {
 // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
 if (e.ctrlKey && e.keyCode === 13) {
 // call your function to do the thing
