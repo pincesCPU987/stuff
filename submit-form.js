0c0d5b9fee467f2ce73123b6b3072246bf9baccb
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-10-29 11:27:48 -0400

    Delete submit-form.js

--- submit-form.js
+++
@@ -1,9 +0,0 @@
-function doc_keyUp(buttonName,e) {
-// this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
-if (e.ctrlKey && e.keyCode === 13) {
-// call your function to do the thing
-document.getElementById(buttonName).click();
-}
-}
-// register the handler 
-document.addEventListener('keyup', doc_keyUp, false);
