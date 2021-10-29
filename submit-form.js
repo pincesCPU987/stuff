93aa0cb5a2185324b0fa4ccaf7468f9e92cb62c7
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-10-29 11:10:20 -0400

    Create submit-form.js

---
+++ submit-form.js
@@ -0,0 +1,9 @@
+function doc_keyUp(e) {
+// this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
+if (e.ctrlKey && e.keyCode === 13) {
+// call your function to do the thing
+document.getElementById("buttonD").click();
+}
+}
+// register the handler 
+document.addEventListener('keyup', doc_keyUp, false);
