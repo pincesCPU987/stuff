5ba93e0eccb167995ec520fafda483bbbedfd66f
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2021-11-01 13:21:59 -0400

    Update notify.js

--- notify.js
+++ notify.js
@@ -8,15 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
   Notification.requestPermission();
 });
 
-window.onunload(if (Notification.permission !== 'granted'){
-  Notification.requestPermission();
-} else {
-  var notification = new Notification() {
-   icon: 'https://docs.google.com/uc?export=download&id=1Hz5g7Cyuwo49jpDI0i45BiYBlD1W-yNC',
-   body: 'Click here on this notification to go back to my site.',
-  }
-  notification.onclick = function() {
-   window.open('https://pincescpu987.github.io/Links/');
-  }
- }
-);
+window.onbeforeunload = notifyMe;
+function notifyMe(e){
+   // do something...
+   return null;
+}
