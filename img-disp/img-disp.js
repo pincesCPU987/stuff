0325bfb1d0d4d33f0cca51fcaae82e884e99cbec
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2022-02-08 12:19:53 -0500

    Update img-disp.js

--- img-disp/img-disp.js
+++ img-disp/img-disp.js
@@ -1,4 +1,7 @@
-var loadFile = function(event) {
-	var image = document.getElementById('output');
-	image.src = URL.createObjectURL(event.target.files[0]);
+function loadFile(){
+  var loadFile = function(event) {
+  var image = document.getElementById('output');
+  var fileSrc = URL.createObjectURL(event.target.files[0]);
+  localStorage.setItem('imgDispFile', fileSrc)
+  image.src = localStorage.getItem
 }
